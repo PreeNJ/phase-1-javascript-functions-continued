0058851812a87f5da3994425cbb54c77c1095f9b
Priscillah Njai <priscillahnjaii@gmail.com>
2025-03-13 22:21:52 +0300

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  function mondayWork(activity = 'go to the office') {
+    console.log(`This Monday, I will ${activity}`);
+    return `This Monday, I will ${activity}`;
+  }
+  
+
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+
+  console.log(saturdayFun());
+  console.log(saturdayFun("bathe my dog"));
+  console.log(mondayWork());
+  console.log(mondayWork("work from home"));
+  
+  const startPrompt = wrapAdjective("*");
+  console.log(startPrompt("amazing"));
+  
+  const pipePrompt = wrapAdjective("|");
+  console.log(pipePrompt("amazing")); 
+  
+  const exclamationPrompt = wrapAdjective("!!!");
+  console.log(exclamationPrompt("dedicated programmer"));
